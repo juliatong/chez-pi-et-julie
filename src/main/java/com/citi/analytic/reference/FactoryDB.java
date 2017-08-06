a42ff2e79e0f468f7dfaf03b33d99978d1e543dd
Pijianxi <pjsh@cn.ibm.com>
2017-08-06 23:00:16 +0800

    远程数据配置

--- src/main/java/com/citi/analytic/reference/FactoryDB.java
+++ src/main/java/com/citi/analytic/reference/FactoryDB.java
@@ -8,9 +8,12 @@ public class FactoryDB {
 
 	public  static Connection getConnection() {
 	    String driver = "oracle.jdbc.OracleDriver";
-	    String url = "jdbc:oracle:thin:@localhost:1521:UAT";
+//	    String url = "jdbc:oracle:thin:@localhost:1521:UAT";
+	    //oracle cloud database service(dbcs) url
+	    String url="jdbc:oracle:thin:@(DESCRIPTION=(ADDRESS=(HOST=129.150.70.18)(PORT=1521)(PROTOCOL=tcp))(CONNECT_DATA=(SERVICE_NAME=PDB1.a512388.oraclecloud.internal)))";
 	    String user = "diamond";
-	    String password = "diamond2017";
+//	    String password = "diamond2017";
+	    String password ="password123";
 	    Connection conn = null;
 	    try {
 	        Class.forName(driver); //classLoader,加载对应驱动
