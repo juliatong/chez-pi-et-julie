460a7c216c52e22e3ca4e8653b735624559b87e5
XH <XH@XH-PC>
2017-12-06 18:40:21 +0800

    git trial

--- src/test/java/com/citi/analytic/dao/TestMybatis.java
+++ src/test/java/com/citi/analytic/dao/TestMybatis.java
@@ -15,7 +15,7 @@ public class TestMybatis {
 		ApplicationContext context=new ClassPathXmlApplicationContext("mybatis/mybatis-generic-dao.xml");
 		MybatisGenericDao<Entity, Criteria> mybatisDao=(MybatisGenericDao)context.getBean("mybatisGenericDao");
 		User instance=new User();
-	//	instance.setUserId("111");
+	//	123instance.setUserId("111");
 		instance.setAddress("aa");
 		instance.setEmail("###");
 		instance.setFirstname("jj");
